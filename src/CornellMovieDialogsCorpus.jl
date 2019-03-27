4dc2e15d31bf42277bb6aeba085a6733bbf70230
David Ellison <davidehellison@gmail.com>
2019-03-26 23:07:23 -0700

    more docs, tests

--- src/CornellMovieDialogsCorpus.jl
+++ src/CornellMovieDialogsCorpus.jl
@@ -2,7 +2,8 @@ module CornellMovieDialogsCorpus
 
 export
     movie_conversations, movie_lines,
-    movie_title_metadata, movie_character_metadata
+    movie_title_metadata, movie_character_metadata,
+    movie_script_urls
 
 using Dates
 using CSV, DataDeps
@@ -33,6 +34,12 @@ end
     movie_conversations(;kwargs...)
 
 Load the conversations file of the movie dialog corpus.
+
+Fields:
+1. :character1_id
+2. :character2_id
+3. :movie_id
+4. :lines
 """
 function movie_conversations(;kwargs...)
     header = [:character1_id, :character2_id, :movie_id, :lines]
@@ -43,6 +50,12 @@ end
     movie_lines(;kwargs...)
 
 Load the lines file of the movie dialog corpus.
+Fields:
+1. :line_id
+2. :character_id
+3. :movie_id
+4. :character_name
+5. :text
 """
 function movie_lines(;kwargs...)
     header = [:line_id, :character_id, :movie_id, :character_name, :text]
@@ -53,6 +66,14 @@ end
     movie_titles_metadata(;kwargs...)
 
 Load the movie title metadata file of the movie dialog corpus.
+
+Fields:
+1. :movie_id
+2. :title
+3. :year
+4. :rating
+5. :votes
+6. :genres
 """
 function movie_title_metadata(;kwargs...)
     header = [:movie_id, :title, :year, :rating, :votes, :genres]
@@ -65,6 +86,14 @@ end
     movie_character_metadata(;kwargs...)
 
 Load the character metadata file of the movie dialog corpus.
+
+Fields:
+1. :character_id
+2. :name
+3. :movie_id
+4. :title
+5. :gender
+6. :position
 """
 function movie_character_metadata(;kwargs...)
     header = [:character_id, :name, :movie_id, :title, :gender, :position]
@@ -75,6 +104,11 @@ end
     movie_script_urls(;kwargs...)
 
 Load the urls file of the movie dialog corpus.
+
+Fields:
+1. :movie_id
+2. :movie_title
+3. :url
 """
 function movie_script_urls(;kwargs...)
     header = [:movie_id, :movie_title, :url]
@@ -98,6 +132,24 @@ function __init__()
     sha = "3bde8a571f615201bc2d2453e22878090719638592f774720eddec739de8c900"
     register(DataDep("CornellMovieDialogsCorpus",
                      """
+                     The Cornell Movie-Dialogs Corpus
+
+                     https://www.cs.cornell.edu/~cristian/Cornell_Movie-Dialogs_Corpus.html
+
+                     This corpus contains a large metadata-rich collection of fictional conversations extracted from raw movie scripts:
+                     - 220,579 conversational exchanges between 10,292 pairs of movie characters
+                     - involves 9,035 characters from 617 movies
+                     - in total 304,713 utterances
+                     - movie metadata included:
+                         - genres
+                         - release year
+                         - IMDB rating
+                         - number of IMDB votes
+                         - IMDB rating
+                     - character metadata included:
+                         - gender (for 3,774 characters)
+                         - position on movie credits (3,321 characters)
+                     - see README.txt (included) for details
                      """,
                      url, sha, post_fetch_method = _unpack))
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,12 @@ using CornellMovieDialogsCorpus, Test
     lines = movie_lines()
     movies = movie_title_metadata()
 
+    @test isfile(CornellMovieDialogsCorpus.readme_file())
+
     @test length(characters) == 9035
     @test length(movies) == 617
     @test length(lines) == 304_713
+
+    @test length(lines) == sum(length.([conv.lines for conv in conversations]))
+    @test length(movie_script_urls()) == length(movies)
 end
